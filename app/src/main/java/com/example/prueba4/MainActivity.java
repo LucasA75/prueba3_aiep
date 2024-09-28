9ac5bc7e4ec568de7e557ecdd4c604c2f1b06d95
LucasA_75 <lucasacevedocastro@gmail.com>
2024-09-28 10:27:38 -0300

    configuracion activity y envio de mensaje entre vistas

--- app/src/main/java/com/example/prueba4/MainActivity.java
+++ app/src/main/java/com/example/prueba4/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.prueba4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText elTexto = findViewById(R.id.editTextText);
+        TextView elMensaje = findViewById(R.id.textView4);
+        Button elBoton = findViewById(R.id.button);
+
+        elBoton.setOnClickListener( new View.OnClickListener() {
+
+            public void onClick(View v) {
+                elMensaje.setText(elTexto.getText());
+                Intent elIntento = new Intent(MainActivity.this, MenuActivity.class);
+                elIntento.putExtra("pasandoElTexto",elTexto.getText().toString());
+                startActivity(elIntento);
+
+            }
         });
+
     }
 }
--- app/src/main/java/com/example/prueba4/MenuActivity.java
+++ app/src/main/java/com/example/prueba4/MenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.prueba4;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,8 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        String texto = getIntent().getExtras().getString("pasandoElTexto");
+        TextView elMensaje = findViewById(R.id.lbl_mensaje_menu);
+        elMensaje.setText(texto);
     }
 }
