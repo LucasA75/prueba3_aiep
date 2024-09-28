c018c8c4b2cee028a11f10f5a2b6cc7250303c02
LucasA_75 <lucasacevedocastro@gmail.com>
2024-09-28 11:06:57 -0300

    se agrega geoLocalizacion

--- app/src/main/java/com/example/prueba4/ClaseJava.java
+++ app/src/main/java/com/example/prueba4/ClaseJava.java
@@ -1,12 +1,38 @@
 package com.example.prueba4;
 
-public class ClaseJava {
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
 
-    public ClaseJava(){
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class ClaseJava extends Activity implements LocationListener {
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
 
     }
 
-    public String hola(){
-        return "Hola Mundo";
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 }
--- app/src/main/java/com/example/prueba4/MainActivity.java
+++ app/src/main/java/com/example/prueba4/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.prueba4;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.Manifest;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    LocationManager locationManager;
+    TextView mensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +47,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new
+                    String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number can be used
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                this);
     }
-}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        mensaje.setText(String.format("%s : %s", location.getLatitude(), location.getLongitude()));
+    }
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+    }
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+    }
+    }
